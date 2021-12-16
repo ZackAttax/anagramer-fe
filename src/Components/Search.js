078bd5afdd22956bbb1f33269006194a8cda9e4f
Zack <zackwilldev@gmail.com>
2021-12-16 11:25:30 -0500

    handleSubmit finds anagrams. handleFormChange takes in searchWord. find Anagrams fetches word from API.

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,19 +2,39 @@ import React, {useState} from "react";
 
 function Search() {
     const [anagrams, setAnagrams] = useState([])
+    const [wordSearch, setWordSearch] = useState("")
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        findAnagrams(wordSearch)
+    }
+    const handleFormChange = event => {
+        setWordSearch(event.target.value)
+    }
+    const findAnagrams = (word) => {
+        fetch(`http://localhost:3000/search/${word}`)
+        .then(res => res.json())
+        .then((results) => {
+            console.log(results)
+            setAnagrams(results)
+        })
+    }
 
     return(
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Search for anagrams
                 </label>
                 <input
                 classname='searchInput'
                 type='text'
-                name='word'
+                name='wordSearch'
+                value={wordSearch} onChange={handleFormChange}
                 />
             </form>
+            Anagrams for {wordSearch} are 
+            {anagrams.map(word => <div>{word}</div> )}
         </div>
     )
 }
